429a4d5c9091fccaf823583311932e2a93303e94
Janio <janiojunior@gmail.com>
2024-02-10 11:16:23 -0300

    first commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into pessoa (id, nome, cpf) values(1, 'Leandra', '111');
+insert into pessoa (id, nome, cpf) values(2, 'Fredson', '222');
+insert into pessoa (id, nome, cpf) values(3, 'Marco', '333');
+insert into pessoa (id, nome, cpf) values(4, 'Carlos Henrique', '444');
