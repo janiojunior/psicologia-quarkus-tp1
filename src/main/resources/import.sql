9a15d79b629590018555c66e0a2e8fe562534f79
Janio <janiojunior@gmail.com>
2024-02-17 11:22:10 -0300

    adicionado o repository

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,3 +2,7 @@ insert into pessoa (id, nome, cpf) values(1, 'Leandra', '111');
 insert into pessoa (id, nome, cpf) values(2, 'Fredson', '222');
 insert into pessoa (id, nome, cpf) values(3, 'Marco', '333');
 insert into pessoa (id, nome, cpf) values(4, 'Carlos Henrique', '444');
+
+
+insert into estado (nome, sigla) values('Tocantins', 'TO');
+insert into estado (nome, sigla) values('Goias', 'GO');
