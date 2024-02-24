5d8f96f7bad12c66ed83512bddc878e0119cd319
Janio <janiojunior@gmail.com>
2024-02-24 10:38:30 -0300

    Crud de estado

--- src/main/java/br/unitins/topicos1/repository/EstadoRepository.java
+++ src/main/java/br/unitins/topicos1/repository/EstadoRepository.java
@@ -1,10 +1,20 @@
 package br.unitins.topicos1.repository;
 
+import java.util.List;
+
 import br.unitins.topicos1.model.Estado;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class EstadoRepository implements PanacheRepository<Estado> {
+
+    public List<Estado> findByNome(String nome) {
+        return find("UPPER(nome) LIKE ?1", "%"+ nome.toUpperCase() + "%").list();
+    }
+
+    public List<Estado> findBySigla(String sigla) {
+        return find("UPPER(sigla) LIKE ?1", "%"+ sigla.toUpperCase() + "%").list();
+    }
     
 }
--- src/main/java/br/unitins/topicos1/resource/EstadoResource.java
+++ src/main/java/br/unitins/topicos1/resource/EstadoResource.java
@@ -7,12 +7,17 @@ import br.unitins.topicos1.repository.EstadoRepository;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 @Path("/estados")
 public class EstadoResource {
     
@@ -20,16 +25,55 @@ public class EstadoResource {
     public EstadoRepository estadoRepository;
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
+
+    @Path("/{id}")
+    public Estado findById(@PathParam("id") Long id) {
+        return estadoRepository.findById(id);
+    }
+
+    @GET
     public List<Estado> findAll() {
         return estadoRepository.listAll();
     }
 
-    @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Transactional
-    public void create(Estado estado) {
-        estadoRepository.persist(estado);
+    @GET
+    @Path("/search/nome/{nome}")
+    public List<Estado> findByNome(@PathParam("nome") String nome) {
+        return estadoRepository.findByNome(nome);
     }
+
+    @GET
+    @Path("/search/sigla/{sigla}")
+    public List<Estado> findBySigla(@PathParam("sigla") String sigla) {
+        
+        return estadoRepository.findBySigla(sigla);
+    }
+
+
+    @POST
+    @Transactional
+    public Estado create(Estado estado) {
+        estadoRepository.persist(estado);
+        return estado;
+    }
+
+    @PUT
+    @Transactional
+    @Path("/{id}")
+    public void update(@PathParam("id") Long id, Estado estado) {
+        Estado estadoBanco =  estadoRepository.findById(id);
+
+        estadoBanco.setNome(estado.getNome());
+        estadoBanco.setSigla(estado.getSigla());
+  
+    }
+
+    @DELETE
+    @Transactional
+    @Path("/{id}")
+    public void delete(@PathParam("id") Long id) {
+        estadoRepository.deleteById(id);
+    }
+
+
 }
